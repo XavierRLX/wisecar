260dd2b92f630bb334c153a0776848f41e01d1e3
XavierRLX <91702228+XavierRLX@users.noreply.github.com>
2025-05-01 16:11:05 -0300

    create minhaGarate
    onky folder w page, redirect for page dev

---
+++ src/app/minhaGaregem/page.tsx
@@ -0,0 +1,10 @@
+import UnderDevelopment from "@/components/UnderDevelopment";
+
+export default function FeedPage() {
+  return (
+    <UnderDevelopment
+      redirectLink="/veiculos"
+      redirectText="Voltar para Veículos"
+    />
+  );
+}
